eb76ed1577b419d5580c66ba12e2bb30e15f406b
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-09-07 20:51:53 +0200

    feat: Add list, delete and upsert to CLI, switch CLI from Postgres API to REST API

--- cmd/atmosfeed-client/main.go
+++ cmd/atmosfeed-client/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
-	"log"
+	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/bluesky-social/indigo/api/atproto"
 	"github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/lex/util"
 	"github.com/bluesky-social/indigo/xrpc"
-	"github.com/pojntfx/atmosfeed/pkg/persisters"
 )
 
 const (
@@ -24,7 +26,7 @@ func main() {
 	username := flag.String("username", "example.bsky.social", "Bluesky username (ignored if `--publish` is not provided)")
 	password := flag.String("password", "", "Bluesky password, preferably an app password (get one from https://bsky.app/settings/app-passwords, ignored if `--publish` is not provided)")
 
-	postgresURL := flag.String("postgres-url", "postgresql://postgres@localhost:5432/atmosfeed?sslmode=disable", "PostgreSQL URL")
+	atmosfeedURL := flag.String("atmosfeed-url", "http://localhost:1337", "Atmosfeed server URL")
 
 	feedGeneratorDID := flag.String("feed-generator-did", "did:web:atmosfeed-feeds.serveo.net", "DID of the feed generator (typically the hostname of the publicly reachable URL) (ignored for `--delete`)")
 
@@ -34,12 +36,18 @@ func main() {
 	feedClassifier := flag.String("feed-classifier", "out/local-trending-latest.scale", "Path to the feed classifier to upload (ignored for `--delete` or if `--publish` is not provided)")
 
 	delete := flag.Bool("delete", false, "Whether to delete instead of upsert a feed/classifier")
-	publish := flag.Bool("publish", true, "Whether to publish/unpublish the feed to/from Blueksy")
+	publish := flag.Bool("publish", false, "Whether to publish/unpublish the feed to/from Blueksy")
+	list := flag.Bool("list", false, "Whether to just list available feeds in Atmosfeed and then exit")
 
 	flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	u, err := url.Parse(*atmosfeedURL)
+	if err != nil {
+		panic(err)
+	}
 
 	auth := &xrpc.AuthInfo{}
 
@@ -62,19 +70,6 @@ func main() {
 	auth.Handle = session.Handle
 	auth.Did = session.Did
 
-	log.Println("Connected to PDS", *pdsURL)
-
-	var persister *persisters.Persister
-	if *publish {
-		persister = persisters.NewPersister(*postgresURL)
-
-		if err := persister.Init(false); err != nil {
-			panic(err)
-		}
-
-		log.Println("Connected to PostgreSQL")
-	}
-
 	rec := &util.LexiconTypeDecoder{
 		Val: &bsky.FeedGenerator{
 			CreatedAt:   time.Now().Format(time.RFC3339),
@@ -84,51 +79,123 @@ func main() {
 		},
 	}
 
-	if *delete {
-		if err := atproto.RepoDeleteRecord(ctx, client, &atproto.RepoDeleteRecord_Input{
-			Collection: lexiconFeedGenerator,
-			Repo:       auth.Did,
-			Rkey:       *feedRkey,
-		}); err != nil {
+	if *list {
+		u := u.JoinPath("admin", "feeds")
+
+		q := u.Query()
+		q.Add("rkey", *feedRkey)
+		u.RawQuery = q.Encode()
+
+		req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+		if err != nil {
 			panic(err)
 		}
 
-		if *publish {
-			if err := persister.DeleteFeed(ctx, auth.Did, *feedRkey); err != nil {
-				panic(err)
-			}
-		}
-	} else {
-		if *publish {
-			b, err := os.ReadFile(*feedClassifier)
-			if err != nil {
-				panic(err)
-			}
+		req.Header.Set("Authorization", "Bearer "+auth.AccessJwt)
 
-			if err := persister.UpsertFeed(ctx, auth.Did, *feedRkey, b); err != nil {
-				panic(err)
-			}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		feeds := []string{}
+		if err := json.NewDecoder(resp.Body).Decode(&feeds); err != nil {
+			panic(err)
 		}
 
-		ex, err := atproto.RepoGetRecord(ctx, client, "", lexiconFeedGenerator, auth.Did, *feedRkey)
-		if err == nil {
-			if _, err := atproto.RepoPutRecord(ctx, client, &atproto.RepoPutRecord_Input{
+		if resp.StatusCode != http.StatusOK {
+			panic(resp.Status)
+		}
+
+		fmt.Println(strings.Join(feeds, ","))
+
+		return
+	}
+
+	if *delete {
+		if *publish {
+			if err := atproto.RepoDeleteRecord(ctx, client, &atproto.RepoDeleteRecord_Input{
 				Collection: lexiconFeedGenerator,
 				Repo:       auth.Did,
 				Rkey:       *feedRkey,
-				Record:     rec,
-				SwapRecord: ex.Cid,
 			}); err != nil {
 				panic(err)
 			}
-		} else {
-			if _, err := atproto.RepoCreateRecord(ctx, client, &atproto.RepoCreateRecord_Input{
-				Collection: lexiconFeedGenerator,
-				Repo:       auth.Did,
-				Rkey:       feedRkey,
-				Record:     rec,
-			}); err != nil {
-				panic(err)
+		}
+
+		u := u.JoinPath("admin", "feeds")
+
+		q := u.Query()
+		q.Add("rkey", *feedRkey)
+		u.RawQuery = q.Encode()
+
+		req, err := http.NewRequest(http.MethodDelete, u.String(), nil)
+		if err != nil {
+			panic(err)
+		}
+
+		req.Header.Set("Authorization", "Bearer "+auth.AccessJwt)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			panic(resp.Status)
+		}
+	} else {
+		f, err := os.Open(*feedClassifier)
+		if err != nil {
+			panic(err)
+		}
+
+		u := u.JoinPath("admin", "feeds")
+
+		q := u.Query()
+		q.Add("rkey", *feedRkey)
+		u.RawQuery = q.Encode()
+
+		req, err := http.NewRequest(http.MethodPut, u.String(), f)
+		if err != nil {
+			panic(err)
+		}
+
+		req.Header.Set("Authorization", "Bearer "+auth.AccessJwt)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			panic(resp.Status)
+		}
+
+		if *publish {
+			ex, err := atproto.RepoGetRecord(ctx, client, "", lexiconFeedGenerator, auth.Did, *feedRkey)
+			if err == nil {
+				if _, err := atproto.RepoPutRecord(ctx, client, &atproto.RepoPutRecord_Input{
+					Collection: lexiconFeedGenerator,
+					Repo:       auth.Did,
+					Rkey:       *feedRkey,
+					Record:     rec,
+					SwapRecord: ex.Cid,
+				}); err != nil {
+					panic(err)
+				}
+			} else {
+				if _, err := atproto.RepoCreateRecord(ctx, client, &atproto.RepoCreateRecord_Input{
+					Collection: lexiconFeedGenerator,
+					Repo:       auth.Did,
+					Rkey:       feedRkey,
+					Record:     rec,
+				}); err != nil {
+					panic(err)
+				}
 			}
 		}
 	}
