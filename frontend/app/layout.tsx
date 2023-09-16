3bc15fd079d97a2a425e9c77b57dab9b9f43f209
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-09-16 23:18:44 +0200

    feat: Start implementation of simple Next and shadcn-based frontend for feed management

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,28 @@
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Atmosfeed",
+  description:
+    "Create fully custom Bluesky feeds with Wasm modules, powered by Scale Functions.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,294 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DropdownMenuLink } from "@/components/ui/dropdown-menu-link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Edit,
+  Laptop,
+  LogOut,
+  Moon,
+  MoonStar,
+  MoreVertical,
+  Plus,
+  Rocket,
+  Sun,
+  Trash,
+  User,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useState } from "react";
+import logoDark from "../assets/logo-dark.svg";
+import logoLight from "../assets/logo-light.svg";
+
+export default function Home() {
+  const { setTheme } = useTheme();
+  const [cardCount, setCardCount] = useState(1);
+  const [createFeedDialogOpen, setCreateFeedDialogOpen] = useState(false);
+
+  return (
+    <>
+      <div className="fixed w-full">
+        <header className="container flex justify-between items-center py-6">
+          <Image
+            src={logoDark}
+            alt="Atmosfeed Logo"
+            className="h-10 w-auto mr-4 logo-dark"
+          />
+
+          <Image
+            src={logoLight}
+            alt="Atmosfeed Logo"
+            className="h-10 w-auto mr-4 logo-light"
+          />
+
+          <div className="flex content-center">
+            <Dialog
+              onOpenChange={(v) => setCreateFeedDialogOpen(v)}
+              open={createFeedDialogOpen}
+            >
+              <DialogTrigger asChild>
+                <Button
+                  className="mr-4"
+                  onClick={() => setCreateFeedDialogOpen((v) => !v)}
+                >
+                  <Plus className="sm:mr-2 h-4 w-4" />{" "}
+                  <span className="hidden sm:inline">Create Feed</span>
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Create Feed</DialogTitle>
+                  <DialogDescription>
+                    Build a new custom Bluesky feed.
+                  </DialogDescription>
+                </DialogHeader>
+
+                <div className="grid gap-4 py-4">
+                  <div className="grid w-full gap-4">
+                    <Label htmlFor="title">Title</Label>
+                    <Input id="title" value="Atmosfeed Trending" />
+                  </div>
+
+                  <div className="grid w-full gap-4">
+                    <Label htmlFor="key">Key</Label>
+                    <Input id="key" value="trending" />
+                  </div>
+
+                  <div className="grid w-full gap-4">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea
+                      id="description"
+                      placeholder="Most popular trending posts on Bluesky in the last 24h (testing feed)"
+                    />
+                  </div>
+                </div>
+
+                <DialogFooter>
+                  <Button
+                    onClick={() => {
+                      setCardCount((v) => v + 1);
+                      setCreateFeedDialogOpen((v) => !v);
+                    }}
+                  >
+                    <Plus className="sm:mr-2 h-4 w-4" /> Create Feed
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/pojntfx.png"
+                    alt="@pojntfx"
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuLink
+                  href="https://bsky.app/profile/felicitas.pojtinger.com"
+                  target="_blank"
+                >
+                  <User className="mr-2 h-4 w-4" /> Profile
+                </DropdownMenuLink>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" /> Logout
+                </DropdownMenuItem>
+
+                <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>
+                    <MoonStar className="mr-2 h-4 w-4" />
+                    <span>Theme</span>
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuItem onClick={() => setTheme("light")}>
+                        <Sun className="mr-2 h-4 w-4" /> Light
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        <Moon className="mr-2 h-4 w-4" /> Dark
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("system")}>
+                        <Laptop className="mr-2 h-4 w-4" /> System
+                      </DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </header>
+
+        <div className="gradient-blur">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className="gradient-blur-bottom">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="content">
+        <main className="flex-grow flex flex-col justify-center items-center gap-2 container">
+          {[...Array(cardCount).keys()].map((_, i) => (
+            <Card
+              className="w-full max-w-2xl flex items-center justify-between"
+              key={i}
+            >
+              <CardHeader>
+                <div className="text-2xl font-semibold leading-none tracking-tight flex items-center justify-between">
+                  <div>Atmosfeed Trending {i + 1}</div>
+
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild className="sm:hidden">
+                      <Button variant="ghost" size="icon">
+                        <MoreVertical />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                          <Edit className="mr-2 h-4 w-4" /> Edit
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem>
+                          <Rocket className="mr-2 h-4 w-4" /> Publish
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem>
+                          <Trash className="mr-2 h-4 w-4" /> Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+                <CardDescription>
+                  <code>trending-{i + 1}</code>
+                </CardDescription>
+                <CardDescription>
+                  Most popular trending posts on Bluesky in the last 24h
+                  (testing feed)
+                </CardDescription>
+              </CardHeader>
+
+              <CardFooter className="py-0 pr-4 hidden sm:flex">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <MoreVertical />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        <Edit className="mr-2 h-4 w-4" /> Edit
+                      </DropdownMenuItem>
+
+                      <DropdownMenuItem>
+                        <Rocket className="mr-2 h-4 w-4" /> Publish
+                      </DropdownMenuItem>
+
+                      <DropdownMenuItem>
+                        <Trash className="mr-2 h-4 w-4" /> Delete
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </CardFooter>
+            </Card>
+          ))}
+        </main>
+      </div>
+
+      <div className="fixed bottom-0 w-full">
+        <footer className="flex justify-between items-center py-6 container">
+          <a
+            href="https://github.com/pojntfx/atmosfeed"
+            target="_blank"
+            className="hover:underline whitespace-nowrap mr-4"
+          >
+            © 2023 Felicitas Pojtinger
+          </a>
+
+          <a
+            href="https://felicitas.pojtinger.com/imprint"
+            target="_blank"
+            className="hover:underline"
+          >
+            Imprint
+          </a>
+        </footer>
+      </div>
+    </>
+  );
+}
